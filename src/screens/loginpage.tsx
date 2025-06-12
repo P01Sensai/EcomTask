dea16b812235da6d49952be4646bfb511e391c66
P01Sensai <pramanshuprajapati@gmil.com>
2025-06-12 19:08:47 +0530

    refactor: restructure project by moving App component to src directory and adding homepage and loginpage components

---
+++ src/screens/loginpage.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const loginpage = () => {
+  return (
+    <View>
+      <Text>loginpage</Text>
+    </View>
+  )
+}
+
+export default loginpage
+
+const styles = StyleSheet.create({})
